02edcd9d10d8943ff76216f1c4126c49e99a3114
DillonTheVillain <dillonsmiles@hotmail.com>
2014-12-01 22:36:09 +0000

    Update Mercurian.java

--- Invaders/src/Mercurian.java
+++ Invaders/src/Mercurian.java
@@ -55,19 +55,7 @@ public Mercurian(int parentWidth, int parentHeight)
 			g.drawImage(img, position.x, position.y, width, height, null);
 			}
 			
-			//this code uses an iterator to run through the bullets, if a bullet has bee maked as inactive it is removed,
-			//otherwise the bullets move and draw methods are called to update it on the screen
-			Iterator<EnemyBullet> iterator = bulletList.iterator();
-			while (iterator.hasNext()) {
-			    EnemyBullet b = iterator.next();
-			    
-			    if(b!=null && b.isActive()){
-			    	b.move();
-			    	b.draw(g);
-			    }
-			    else{
-			    	iterator.remove();
-			    }
+		
 			}
 		}
 		
