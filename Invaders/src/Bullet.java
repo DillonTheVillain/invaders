846f9856ebc0160dfdeeccd8a3662e9a7ee30803
DillonTheVillain <dillonsmiles@hotmail.com>
2014-12-01 18:16:09 +0000

    Update Bullet.java

--- Invaders/src/Bullet.java
+++ Invaders/src/Bullet.java
@@ -53,6 +53,16 @@ public class Bullet {
 		
 	}
 	
+	public void moveEnemy(){
+		if(position.y > 600){
+			destroy();
+		}
+		else{
+			position.y+=speed;
+		}
+		
+	}
+	
 	//sets the bullet as inactive
 	public void destroy(){
 		active = false;
