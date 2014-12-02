ae6272f7599d02004d10665c1fb396233c21525f
DillonTheVillain <dillonsmiles@hotmail.com>
2014-12-02 22:04:53 +0000

    Update Boss.java

--- Invaders/src/Boss.java
+++ Invaders/src/Boss.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 
 abstract class Boss extends Enemy {
 	public Boss(Point point, int parentWidth) {
-		super(point, parentWidth);
+		super(point, parentWidth); //Boss sending point and parentwidth information up to Enemy
 		// TODO Auto-generated constructor stub
 	}
 
