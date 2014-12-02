7e69277ff7ee699e1033ab135e141ff6b3bbccca
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:28:15 +0000

    Update Mercurian.java

--- Invaders/src/Mercurian.java
+++ Invaders/src/Mercurian.java
@@ -1,59 +1,69 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.swing.Timer;
 
 
-public class Martian extends Invader{
-	private ArrayList<EnemyBullet> bulletList;
-	private int points = 40;
-	private BufferedImage img;
-	private Point position = new Point(0,0);
-	private boolean isActive=true;
-	private int lives=1;
-	private int width;
-	private int height;
-	private int speed=5;
+public class Mercurian extends Invader implements ActionListener {
 
-	public Martian(Point point,int parentWidth)
-	{
-		super(point,parentWidth);
-		//this code loads the image for the player. The try catches are used to catch and file io error
-		try {
-			img = ImageIO.read(getClass().getResource("/martian.jpg"));
-			System.out.println("***************OK*******************");
-		} catch (IOException e) {
-			System.out.println("***************CAN'T READ FILE*******************");
-			e.printStackTrace();
+private int points = 40;
+private BufferedImage img;
+private Point position;
+private boolean isActive=true;
+private int lives=1;
+private int width;
+private int height;
+private int speed=5;
+private boolean cloak = false;
+Random rnd=new Random();
+Timer cloakTimer = new Timer (2000,this);
+
+
+
+
+public Mercurian(Point point,int parentWidth)
+{
+	super(point,parentWidth);
+	//this code loads the image for the player. The try catches are used to catch and file io error
+	try {
+		img = ImageIO.read(getClass().getResource("/mercurian.jpg"));
+		System.out.println("***************OK*******************");
+	} catch (IOException e) {
+		System.out.println("***************CAN'T READ FILE*******************");
+		e.printStackTrace();
+	}
+	
+	//the height and width of the player are set via the image size
+	this.height = img.getHeight();
+	this.width = img.getWidth();
+	cloakTimer.start();
+	this.position=point;
+	}
+	
+			public void actionPerformed(ActionEvent e) {
+				{
+					
+					if (e.getSource() == cloakTimer){
+					cloak=!cloak;
+					}}
+				}
+			@Override
+		public void draw(Graphics g) {
+			
+			if(!cloak)
+			{
+			//draws the Destroyer image in its current position if not cloaked
+			g.drawImage(img, position.x, position.y, width, height, null);
+			}
+			
+		
+			}
 		}
 		
-		//the height and width of the player are set via the image size
-		this.height = img.getHeight();
-		this.width = img.getWidth();
-		this.position=point;
-		
-		bulletList = new ArrayList<EnemyBullet>();
-	}
-public void draw(Graphics g) {
-		
-		//draws the player image in its current position
-		g.drawImage(img, position.x, position.y, width, height, null);			
-	}
-public void fire(){
-	bulletList.add(new EnemyBullet(new Point(position.x+(width/2), position.y)));
-}
-
-//this returns a specific player bullet
-public EnemyBullet getBullet(int i){
-	return bulletList.get(i);
-	
-}
-//this returns the current number of bullets
-public int getBulletCount(){
-	return bulletList.size();
-}
-}
