5f8f5bce4aea1f0e178f072adbfacb0e6ed3c04c
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:59:26 +0000

    Update canShoot.java

--- Invaders/src/canShoot.java
+++ Invaders/src/canShoot.java
@@ -1,5 +1,19 @@
 
 public interface canShoot {
+private ArrayList<EnemyBullet> bulletList;
+bulletList = new ArrayList<EnemyBullet>();
 
-	public void shoot();
+public void shoot(){
+	bulletList.add(new EnemyBullet(new Point(position.x+(width/2), position.y)));
+}
+
+//this returns a specific player bullet
+public EnemyBullet getBullet(int i){
+	return bulletList.get(i);
+	
+}
+//this returns the current number of bullets
+public int getBulletCount(){
+	return bulletList.size();
+}
 }
