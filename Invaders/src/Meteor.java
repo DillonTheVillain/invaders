9c06ed915641b99f967c0938778058752e393cf7
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:28:43 +0000

    Update Meteor.java

--- Invaders/src/Meteor.java
+++ Invaders/src/Meteor.java
@@ -1,4 +1,4 @@
-import java.awt.Graphics;
+
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -18,9 +18,9 @@ public class Meteor extends Enemy {
 	private int height;
 	Random rnd = new Random();
 	
-	public Meteor(int parentWidth, int parentHeight, int playerSpeed, int score)
+	public Meteor(int parentWidth, int parentHeight,Point point)
 	{
-		super(parentWidth,parentHeight,playerSpeed,score,isActive());//check isActive
+		super(point,parentWidth);
 		try {
 			img = ImageIO.read(getClass().getResource("/Space-invaders.jpg"));//Change to Meteor sprite
 			System.out.println("***************OK*******************");
@@ -34,8 +34,9 @@ public class Meteor extends Enemy {
 		this.width = img.getWidth();
 		this.parentWidth=parentWidth;
 		this.parentHeight=parentHeight;
-		
+		this.position.y=0;
 		this.position.x = rnd.nextInt(parentWidth)+1;
+		point=this.position;
 	}
 	
 	
@@ -45,11 +46,6 @@ public class Meteor extends Enemy {
 			if(position.y != parentHeight)
 			{
 				this.position.y += (10*playerSpeed);
-			}
-			else
-			{
-				Player.die();//Only works if lives + methods are static. Maybe change to check position, then kill player.
-				setActive(false);
 			}
 	}
 }
