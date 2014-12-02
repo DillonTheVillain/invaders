209fe700c72fa8ce33346c17f175965e459b6f13
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:21:43 +0000

    Update Boss.java

--- Invaders/src/Boss.java
+++ Invaders/src/Boss.java
@@ -1,6 +1,15 @@
+import java.awt.Point;
 
-abstract class Boss {
+
+
+abstract class Boss extends Enemy {
+	public Boss(Point point, int parentWidth) {
+		super(point, parentWidth);
+		// TODO Auto-generated constructor stub
+	}
 
 	int lives;
+
+		
+	}
 	
-}
