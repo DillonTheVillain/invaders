0431024f19af8927cd7cce083060e2f73a72e7ad
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:26:13 +0000

    Update Invader.java

--- Invaders/src/Invader.java
+++ Invaders/src/Invader.java
@@ -1,6 +1,42 @@
+import java.awt.Point;
+
 
 abstract class Invader extends Enemy {
-
-	//Stu was here
-	
+private Point point;
+private int playerSpeed=5;
+private int parentWidth;
+boolean lastLeft=true;
+public Invader(Point point,int parentWidth) {
+		super(point,parentWidth);
+		this.point=point;
+		this.parentWidth=parentWidth;
+	}
+		@Override
+		public void move()
+		{
+			if(lastLeft)
+			{
+				if(point.x < 1000)
+				{
+				point.x += (2*playerSpeed);
+				}
+			}
+			if(point.x==1000)
+			{
+				lastLeft=!lastLeft;
+				point.y += 20;
+			}
+			if (!lastLeft)
+			{
+				if(point.x >= 0)
+				{
+					point.x -= (2*playerSpeed);
+				}
+			}
+			if(point.x==0)
+			{
+				lastLeft=!lastLeft;
+				point.y += 20;
+			}
+}	
 }
