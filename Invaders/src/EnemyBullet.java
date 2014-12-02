fbae30fd90a5130f36f44177246da581ff545431
SgtTugs <1304329@abertay.ac.uk>
2014-12-02 21:25:19 +0000

    Update EnemyBullet.java

--- Invaders/src/EnemyBullet.java
+++ Invaders/src/EnemyBullet.java
@@ -2,54 +2,82 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Iterator;
 
 import javax.imageio.ImageIO;
 
 
-
-abstract class Enemy {
+public class EnemyBullet {
 	
-	private int parentWidth;
-	private int parentHeight;
-	private int width;
-	private int height;
 	private Point position;
-	private int playerSpeed =7;
+	private int height, width;
 	private BufferedImage img;
-	boolean isActive=true;
-	private int score;
-	private int lives;
-	private int points;
-	
-	public Enemy(Point point, int parentWidth)
-	{
-		this.position = point;
-	}
+	private int speed = 20;
+	private boolean active;
 
-public void draw(Graphics g) {
+	
+	public EnemyBullet(Point enemyPosition){
 		
-		//draws the player image in its current position
-		g.drawImage(img, position.x, position.y, width, height, null);			
-	}
-
-public void die() {
-	lives--;
-	if(lives==0){
-		isActive=false;
+		//same as the player class this simply loads the image for the bullet.
+		try {
+			img = ImageIO.read(getClass().getResource("/playerbullet.jpg"));
+			//System.out.println("***************OK*******************");
+		} catch (IOException e) {
+			//System.out.println("***************CAN'T READ FILE*******************");
+			e.printStackTrace();
+		}
+		
+		height = img.getHeight();
+		width = img.getWidth();
+		
+		//sets the bullet position we have to do a little offset wit the bullet image so our bullet appears in the (horizontal) centre of our player
+		position = new Point(position.x - (width/2), position.y);
+		
+		active = true;
 	}
 	
-}
+	//draws the bullet
+	public void draw(Graphics g) {
+	
+		g.drawImage(img, position.x, position.y, width, height, null);
+		
+	}
+	
+	//moves the bullet unless it has gone off the top of the screen in which case the bullet is destroyed
+	public void move(){
+		if(position.y > 600){
+			destroy();
+		}
+		else{
+			position.y+=speed;
+		}
+		
+	}
+	
+	
+	
+	//sets the bullet as inactive
+	public void destroy(){
+		active = false;
+	}
+	
+	//returns whether the bullet is active or not
+	public boolean isActive(){
+		return active;
+	}
 
-public int getScore() {
-	return points;
-}
+	//returns the bullets current position
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return position;
+	}
+	
+	// After collision
+//	public void detectCollision(Enemy e)
+//	{
+//		
+//	}
+	
+	
 
-public Point getPosition() {
-	return position;
 }
-
-public void move(){
-}
-}
-
-
